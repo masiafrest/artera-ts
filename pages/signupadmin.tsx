9ed04bc16f7049c1099cc10b8f8deab07f1301dd
masiaf <masiafrest@gmail.com>
2022-08-13 17:54:01 -0500

    del unused var

--- pages/signupadmin.tsx
+++ pages/signupadmin.tsx
@@ -1,27 +1,6 @@
-import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  HStack,
-  InputRightElement,
-  Stack,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
-  Link,
-  useToast,
-} from "@chakra-ui/react";
-import NextLink from "next/link";
+import { Flex } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { useFormFields } from "lib/utils/useFormFields";
-import { supabase } from "lib/utils/supabaseClient";
 import { SupabaseAuthPayload } from "lib/types";
-import { useAuth } from "lib/context/AuthContext";
 import SignUpForm from "components/Forms/SignupForm";
 
 const FORM_VALUES: SupabaseAuthPayload = {
